dda1d93feda9142902c5df6a0a9c78ea08f8db75
scollinspt <scollinspt@tutanota.com>
2024-05-31 19:07:23 -0400

    fixed Ex1.2.19

--- Ex1219.java
+++ Ex1219.java
@@ -7,11 +7,11 @@ public class Ex1219
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		double r = Math.random();
-		int c = a + (int)(r*b);
+		int c = a + (int)(r*(b-a));
 		System.out.println("lower bound is: " + a);
 		System.out.println("upper bound is: " + b);
 		System.out.println("random number between 0 and 1 is " + r);
-		System.out.println("r * b is " + (int)(r*b));
+		System.out.println("r * (b-a) is " + (int)(r*(b-a)));
 		System.out.println("A random integer between " + a + " and " + b + " is " + c);
 	}
 }
